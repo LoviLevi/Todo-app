264a723b672a3c1b5493599f86a195cfa8fa7232
LoviLevi <levente0518@gmail.com>
2025-02-09 19:59:07 +0100

    task model created and props + ctor added

---
+++ Model/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public String Description { get; set; }
+        public int Expiring_time { get; set; }
+
+        public Task(string description, int expiring_time)
+        {
+            Description = description;
+            Expiring_time = expiring_time;
+        }
+    }
+   
+}
